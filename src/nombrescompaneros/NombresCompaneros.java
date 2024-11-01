7bfa1981dcdc4d9d128753f35945ce5574adf26e
CARLOS PORTILLO <CARLOS PORTILLO@PORTILLO>
2024-11-01 12:11:26 -0600

    inicio

---
+++ src/nombrescompaneros/NombresCompaneros.java
@@ -0,0 +1,11 @@
+package nombrescompaneros;
+public class NombresCompaneros {
+    public static void main(String[] args) {
+        String[] nombres = {"Carlos", "Daniela", "Evelyn", "Blanca", "Sindy", "Diana", "Keysi", "Andrea", "Arnold", "Felipe"};
+        
+        for (String nombre : nombres) {
+            System.out.println(nombre);
+        }
+    }
+}
+
